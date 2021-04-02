4a68ecf77a317c34c53d28f8fa238f9b1d82ca85
mattwheeler-shi <78182517+mattwheeler-shi@users.noreply.github.com>
2021-04-01 21:53:07 -0500

    Update prep.bash

--- prep.bash
+++ prep.bash
@@ -1,13 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 apt update
-apt -y upgrade
-apt -y install perl* wireshark-common tcpdump tshark ethtool
-
+apt upgrade
+apt install perl* wireshark-common tcpdump tshark ethtool
 mkdir /opt/threatReplayer
 cd /opt/threatReplayer
 git clone https://github.com/mattwheeler-shi/setup.git
 mv setup/* /opt/threatReplayer
+chmod 775 /opt/threatReplayer/*.bash
+chmod a+x /opt/threatReplayer/*.bash
 tar -xzvf ./TPR_v1.3.5.tgz
 mkdir /PCAPS
 cd /
@@ -15,3 +16,17 @@ git clone https://github.com/mattwheeler-shi/2009-2013.git
 git clone https://github.com/mattwheeler-shi/Demo_Pcaps.git
 mv /Demo_Pcaps/PCAPS/* /PCAPS
 mv /2009-2013/* /PCAPS
+echo "network: {config: disabled}" > /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
+echo
+echo
+echo
+echo
+echo "Reboot"
+echo
+echo
+echo "After reboot, change the /etc/netplan file, then  sudo netplan generate, sudo netplan apply "
+echo
+echo " And run   sudo ip link set eth1 promisc on && sudo ip link set eth2 promisc on"
+echo
+echo
+echo
